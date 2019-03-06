c61e57037ebc9ff897674a257b72429d2d92eae5
Hugo Florez <hfstails@gmail.com>
2019-03-06 22:11:30 +0100

    First version Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/main_StartupNameGenerator.dart';
-import 'package:flutter_app/main_UI.dart';
-import 'package:flutter_app/main_Firebase.dart';
+import 'login_signup_page.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Navigation Basics',
-    home: FirstRoute(),
-  ));
-}
+void main() => runApp(new MyApp());
 
-class FirstRoute extends StatelessWidget {
+class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Page'),
-      ),
-      body: new Container(
-        child: new Column(
-            children: <Widget>[
-              Center(
-                child: RaisedButton(
-                  child: Text('Startup Name Generator'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => StartupNameGenerator()),
-                    );
-                  },
-                ),
-              ),
-              Center(
-                child: RaisedButton(
-                  child: Text('Friendly Chat'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FriendlyChatApp()),
-                    );
-                  },
-                ),
-              ),
-              Center(
-                child: RaisedButton(
-                  child: Text('Baby Name Votes'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => BabyNameVotes()),
-                    );
-                  },
-                ),
-              ),
-            ]
+    return new MaterialApp(
+        title: 'Flutter Login Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
-  }
-}
-
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+        home: new LoginSignUpPage()
     );
   }
 }
