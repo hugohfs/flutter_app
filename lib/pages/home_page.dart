5e37f0efdb1e1e19f6fe48228c9b5063f49712e2
Hugo Florez <hfstails@gmail.com>
2019-03-30 15:20:13 +0100

    Add BottomNavigationBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,8 @@ import 'package:hfs_flutter_app/pages/open_food_facts.dart';
 import 'package:hfs_flutter_app/pages/startup_name_generator.dart';
 import 'package:hfs_flutter_app/pages/todo_page.dart';
 import 'package:hfs_flutter_app/services/authentication.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'
+  as font_awesome_flutter;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.auth, this.userId, this.onSignedOut})
@@ -24,8 +26,17 @@ class _HomePageState extends State<HomePage> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   bool _isEmailVerified = false;
-
   String _userAccountEmail = "";
+
+  int _selectedIndex = 1;
+  /*final _widgetOptions = [
+    Text('Index 0: Home'),
+    Text('Index 1: Startup Name Generator'),
+    Text('Index 2: Friendly Chat'),
+    Text('Index 3: Baby Name Votes'),
+    Text('Index 4: Todo List'),
+    Text('Index 5: Open Food Facts'),
+  ];*/
 
   @override
   void initState() {
@@ -154,16 +165,85 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       )),
-      body: new Container(
-        child: new Column(children: <Widget>[
+      body: Center(
+        child: _showCurrentTab(_selectedIndex)
+          //_widgetOptions.elementAt(_selectedIndex)
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem> [
+            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home'), backgroundColor: Theme.of(context).accentColor),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite_border), title: Text('Name Generator'), backgroundColor: Theme.of(context).accentColor),
+            BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text('Chat')),
+            BottomNavigationBarItem(icon: Icon(Icons.child_care), title: Text('Baby Name')),
+            BottomNavigationBarItem(icon: Icon(Icons.list), title: Text('Todo')),
+            BottomNavigationBarItem(icon: Icon(font_awesome_flutter.FontAwesomeIcons.barcode), title: Text('OpenFoodFacts')),
+          ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: _onItemTapped,),
+      );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _showLogo() {
+    return new Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset('assets/flutter-icon.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget _showCurrentTab(int _selectedIndex) {
+    switch (_selectedIndex) {
+      case 0:
+        return _showHome();
+        break;
+      case 1:
+        return StartupNameGenerator();
+        break;
+      case 2:
+        return ChatScreen(auth: widget.auth,);
+        break;
+      case 3:
+        return BabyNameVotes();
+        break;
+      case 4:
+        return TodoPage();
+        break;
+      case 5:
+        return OpenFoodFactsPage();
+        break;
+      default:
+        return Text("Index tab not defined");
+        break;
+
+
+
+    }
+  }
+
+  Container _showHome() {
+    return Container(
+        child: Column(children: <Widget>[
           _showLogo(),
           Padding(
-            padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
-            child: new Center(
+            padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+            child: Center(
               child: RaisedButton(
                 elevation: 5.0,
-                shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(30.0)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
                 color: Colors.blue,
                 child: Text('Startup Name Generator',
                     style: new TextStyle(fontSize: 20.0, color: Colors.white)),
@@ -192,8 +272,8 @@ class _HomePageState extends State<HomePage> {
                     context,
                     MaterialPageRoute(
                         builder: (context) => ChatScreen(
-                              auth: widget.auth,
-                            )),
+                          auth: widget.auth,
+                        )),
                   );
                 },
               ),
@@ -258,21 +338,6 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         ]),
-      ),
-    );
-  }
-
-  Widget _showLogo() {
-    return new Hero(
-      tag: 'hero',
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
-        child: CircleAvatar(
-          backgroundColor: Colors.transparent,
-          radius: 48.0,
-          child: Image.asset('assets/flutter-icon.png'),
-        ),
-      ),
-    );
+      );
   }
 }
